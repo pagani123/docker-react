ac719a415e1482b28cc5a670782b73a33969e175
pagani123 <94129861+pagani123@users.noreply.github.com>
2022-03-22 18:24:26 +0400

    app.js commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          this is war
-        </p>
+        <p>this is war</p>
         <a
           className="App-link"
           href="https://reactjs.org"
