658ca209df0f7b5395dd16a7630781f0b914d62e
pagani123 <tornike.papa@gmail.com>
2022-03-22 16:27:21 +0400

    App.js was edited

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Es aris kargi rame
+          this is war
         </p>
         <a
           className="App-link"
